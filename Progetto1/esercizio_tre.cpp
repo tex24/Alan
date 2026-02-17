66e40f1cf7d0f37734aee93443ce88c2f4ef6a6b
Francesco Tacchino <tacchifra@gmail.com>
2026-02-17 16:24:59 +0100

    fix

--- Progetto1/esercizio_tre.cpp
+++ Progetto1/esercizio_tre.cpp
@@ -5,10 +5,14 @@ using namespace std;
 int main(int argc, char *argv[]){
 	int d_d = 0, d_f = 0;
 
-	while(powf(2, -d_f) + 1 > 1) d_f++;
-	while(1 + pow(2, -d_d) > 1) d_d++;
-	
-	printf("Max d for double: %d;\nMax d for float: %d;\n", d_d, d_f);
+	while(1 + powf(2, -d_f) > 1) d_f++;
+	while(1 + pow(2, -d_d)  > 1) d_d++;
+
+	const double eps_d = pow(2, -(d_d - 1));
+	const float  eps_f = powf(2, -(d_f - 1));
+
+	printf("Max d for double: %d, eps: %.15e;\nMax d for float: %d, eps: %.7e;\n",
+		   d_d - 1, eps_d, d_f - 1, eps_f);
 
 	return 0;
 }
